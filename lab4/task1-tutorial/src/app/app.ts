3589286e4f2216d30c6b9277491c7d59834c1d04
Танзиля <tanzilya@Noutbuk-Tanzila.local>
2026-02-25 01:07:47 +0500

    Next step of tutorial

--- lab4/task1-tutorial/src/app/app.ts
+++ lab4/task1-tutorial/src/app/app.ts
@@ -1,20 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { User } from './user/user';
-import { Child } from './child/child';
+import { Component } from '@angular/core';
 import { Comments } from './comments/comments';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, User, Child, Comments],
   templateUrl: './app.html',
-  styleUrl: './app.css'
-  
+  styleUrls: ['./app.css'],
+  imports: [Comments]
 })
 export class App {
-  items: string[] = [];                    // ← И ЭТУ СТРОКУ ДОБАВЬ (массив для хранения черепашек)
-
-  addItem(item: string) {                  // ← И ЭТУ ФУНКЦИЮ ДОБАВЬ
-    this.items.push(item);                  // добавляем черепашку в массив
-  }
+  // никаких дополнительных данных не нужно
 }
