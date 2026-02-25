6743caf642b088a3ec062fc94e769a0636679b2b
Танзиля <tanzilya@Noutbuk-Tanzila.local>
2026-02-25 15:45:28 +0500

    Add turtle, forms and routing

--- lab4/task1-tutorial/src/app/app.ts
+++ lab4/task1-tutorial/src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { Comments } from './comments/comments';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Child } from './child/child';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
-  imports: [Comments]
+  imports: [ReactiveFormsModule, Child]
 })
 export class App {
-  // никаких дополнительных данных не нужно
+  items: string[] = [];
+  
+  profileForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+
+  handleSubmit() {
+    alert(this.profileForm.value.name + ' | ' + this.profileForm.value.email);
+  }
+
+  addItem(item: string) {
+    this.items.push(item);
+  }
 }
--- lab4/task1-tutorial/src/app/user/user.ts
+++ lab4/task1-tutorial/src/app/user/user.ts
@@ -1,9 +1,16 @@
 import { Component, input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.html',
-  styleUrls: ['./user.css']
+  styleUrls: ['./user.css'],
+  imports: [NgOptimizedImage]  
 })
 export class User {
   name = input.required<string>();
+  username = 'youngTech';
+  logoUrl = 'logo.svg';  // тоже без /
+  logoAlt = 'Angular logo';
 }
